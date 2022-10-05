5304c5c4184c6b537e8eb1175b4d1f3049870663
Jose Plasencia <josejpd3@gmail.com>
2022-10-05 17:59:25 -0400

    created varibles for sections and buttons

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// Main Screen
+let mainScreen = document.querySelector('.main');
+let startBtn = document.querySelector('#btn-start');
+
+// Questions Screen
+let questionScreen = document.querySelector('#questions-section');
+let questionElement = document.querySelector('#question-title');
+let choicesElement = document.querySelector('#choices');
+
+// Final Score Screen
+let scoreScreen = document.querySelector('#end-screen');
+let finalScoreElement = document.querySelector('#final-score');
+let initialsInput = document.querySelector('#initials');
+let submitInitial = document.querySelector('#submit');
+
+
+// Main Screen
+
+
+
+
+
+// Questions Screen
+
+
+
+
+
+// Final Score Screen
