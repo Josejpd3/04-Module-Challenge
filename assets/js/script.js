bff67f11512c12e572607c77f0581071081a81d0
Jose Plasencia <josejpd3@gmail.com>
2022-10-05 20:56:39 -0400

    created get currentQuestion and timer countdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
 // Main Screen
 let mainScreen = document.querySelector('.main');
 let startBtn = document.querySelector('#btn-start');
+let timerElement = document.querySelector('.timer');
 
 // Questions Screen
 let questionScreen = document.querySelector('#questions-section');
 let questionElement = document.querySelector('#question-title');
 let choicesElement = document.querySelector('#choices');
+
+let questionNumber = 0;
+let currentQuestion;
+let time = 120; 
+
 
 // Final Score Screen
 let scoreScreen = document.querySelector('#end-screen');
@@ -14,10 +20,16 @@ let initialsInput = document.querySelector('#initials');
 let submitInitial = document.querySelector('#submit');
 
 
+
+
 // Main Screen
 function startQuiz() {
     mainScreen.classList.add('hide')
     questionScreen.classList.remove('hide')
+
+
+    timer()
+    getQuestion()
 }
 
 
@@ -27,7 +39,72 @@ startBtn.addEventListener("click", function(event) {
     event.preventDefault()
     startQuiz()
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Questions Screen
+function timer() {
+    let timer = setInterval(function() {
+        timerElement.innerHTML = `Time: ${time}`;
+        time--;
+        if (time < 0) {
+            clearInterval(timer);
+        }
+    }, 1000)
+}
+
+
+function getQuestion() {
+
+
+    currentQuestion = questions[questionNumber].title
+    questionElement.textContent = currentQuestion
+
+    for (let i = 0; i < questions[questionNumber].choices.length; i++) {
+        let choices = document.createElement("button")
+        choicesElement.append(choices)
+        choices.classList.add("optionBtn")
+        choices.textContent = questions[questionNumber].choices[i]
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
